872d965950e3c36e1ea3fe2dfc08bc973d92937d
Mahir-H07 <ttxmahir@gmail.com>
2026-05-31 00:13:34 +0600

    window-reset

--- script.js
+++ script.js
@@ -350,3 +350,15 @@ new Swiper(".testimonialSwiper", {
     }
 
 });
+
+
+
+
+
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+
+window.addEventListener('load', () => {
+    window.scrollTo(0, 0);
+});
