657e1c64011e2d0dd35b4dd6503a6096dcad0803
Mahir-H07 <ttxmahir@gmail.com>
2026-06-01 20:35:22 +0600

    reload-fixed

--- script.js
+++ script.js
@@ -300,11 +300,15 @@ new Swiper(".testimonialSwiper", {
 
 
 
-
-if ('scrollRestoration' in history) {
-    history.scrollRestoration = 'manual';
-}
-
-window.addEventListener('load', () => {
-    window.scrollTo(0, 0);
+window.addEventListener("beforeunload", () => {
+    localStorage.setItem("scrollPos", window.scrollY);
 });
+
+window.addEventListener("load", () => {
+    const scrollPos = localStorage.getItem("scrollPos");
+
+    if (scrollPos) {
+        window.scrollTo(0, parseInt(scrollPos));
+    }
+});
+
